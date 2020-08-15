bdb843b791bb224f4d366cc6a88bc5879fdbf0e1
developerep2019 <developerep2019@gmail.com>
2020-08-16 00:12:45 +0600

    created branch

---
+++ threedots.js
@@ -0,0 +1,20 @@
+const ages = [12, 14, 16, 13, 17];
+const ages2 = [15, 16, 12];
+const ages3 = [25, 36, 22, 29];
+const allAges = ages.concat(ages2).concat(ages3).concat([5]);
+
+const allAges2 = [...ages, ...ages2, 899, ...ages3]
+
+// console.log(allAges2);
+
+const business = 650;
+const minister = 450;
+const sochib = 250;
+
+// const maximum  = Math.max(minister , business , sochib)
+
+// console.log(maximum)
+
+const takaPoisa = [450, 650, 250];
+const maximum = Math.min(...takaPoisa);
+console.log(maximum);
